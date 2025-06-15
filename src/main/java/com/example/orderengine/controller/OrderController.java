546d104aba817e42f3c1f0c362390172bb77425c
sonakshi1314 <sonakshi.mahajan22@gmail.com>
2025-06-15 15:28:49 +0530

    updated order controller endpoint

--- src/main/java/com/example/orderengine/controller/OrderController.java
+++ src/main/java/com/example/orderengine/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api/order/evaluate")
 public class OrderController {
 
     private final OrderService service;
